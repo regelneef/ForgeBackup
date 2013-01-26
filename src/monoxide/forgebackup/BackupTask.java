9ceb3daf25c517da950858eb1364eecfb60ec8fb
monoxide <monoxide0184@gmail.com>
2013-01-26 16:17:47 +1100

    Major cleanup for BackupTask

--- src/monoxide/forgebackup/BackupTask.java
+++ src/monoxide/forgebackup/BackupTask.java
@@ -4,6 +4,7 @@ import java.lang.reflect.Field;
 import java.util.Set;
 import java.util.TimerTask;
 
+import monoxide.forgebackup.backup.Backup;
 import net.minecraft.entity.EntityTracker;
 import net.minecraft.entity.EntityTrackerEntry;
 import net.minecraft.entity.player.EntityPlayer;
@@ -11,9 +12,8 @@ import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.WorldServer;
 
 public class BackupTask extends TimerTask {
-	
 	private final MinecraftServer server;
-	public static boolean lastRunHadPlayers = false; 
+	private static boolean lastRunHadPlayers = false; 
 	
 	public BackupTask(MinecraftServer server) {
 		this.server = server;
@@ -30,39 +30,11 @@ public class BackupTask extends TimerTask {
 			lastRunHadPlayers = true;
 		}
 		
-		server.getCommandManager().executeCommand(ForgeBackup.instance(), "backup");
+		Backup backup = new Backup(ForgeBackup.instance().config().getRegularBackupSettings(server));
+		backup.run(ForgeBackup.instance());
 	}
 	
 	public boolean playerLoggedIn() {
-		Field entitySetField = null;
-		
-		for (Field field : EntityTracker.class.getDeclaredFields()) {
-			if (field.getType() == Set.class) {
-				entitySetField = field;
-				entitySetField.setAccessible(true);
-				break;
-			}
-		}
-		
-		if (entitySetField == null) {
-			BackupLog.warning("Couldn't reflect on EntityTracker correctly; continuing and assuming there are players on the server.");
-			return true;
-		}
-		
-		for (WorldServer world : server.worldServers) {
-			try {
-				Set entities = (Set)entitySetField.get(world.getEntityTracker());
-				
-				for (Object entry : entities) {
-					if (((EntityTrackerEntry)entry).myEntity instanceof EntityPlayer) {
-						return true;
-					}
-				}
-			}
-			catch (IllegalArgumentException e) {}
-			catch (IllegalAccessException e) {}
-		}
-		return false;
+		return !server.getConfigurationManager().playerEntityList.isEmpty();
 	}
-
 }
