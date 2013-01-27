939941d0393846fd238d8e9aaa48bbb703e630ec
monoxide <monoxide0184@gmail.com>
2013-01-27 23:21:15 +1100

    Add basic ForgeEssentials compatibility

---
+++ src/monoxide/forgebackup/essentials/ForgeBackupModule.java
@@ -0,0 +1,8 @@
+package monoxide.forgebackup.essentials;
+
+import monoxide.forgebackup.ForgeBackup;
+
+import com.ForgeEssentials.api.modules.FEModule;
+
+@FEModule(name = "Backups", parentMod = ForgeBackup.class, version = "0.4.1", doesOverride = true)
+public class ForgeBackupModule {}
