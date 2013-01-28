74d5247bb57f9e3e275cf98dd3187c370a7aee2d
monoxide <monoxide0184@gmail.com>
2013-01-28 13:49:06 +1100

    Pull in our first external library

--- src/monoxide/forgebackup/coremod/LibraryDownloader.java
+++ src/monoxide/forgebackup/coremod/LibraryDownloader.java
@@ -5,16 +5,20 @@ import cpw.mods.fml.relauncher.ILibrarySet;
 public class LibraryDownloader implements ILibrarySet {
 	@Override
 	public String[] getLibraries() {
-		return new String[] {};
+		return new String[] {
+			"commons-compress-1.4.1.jar",
+		};
 	}
 
 	@Override
 	public String[] getHashes() {
-		return new String[] {};
+		return new String[] {
+			"a045661155918847e731465645e23ad915d5fc71",
+		};
 	}
 
 	@Override
 	public String getRootURL() {
-		return "";
+		return "https://github.com/monoxide0184/forgebackup/raw/master/libs/%s";
 	}
 }
