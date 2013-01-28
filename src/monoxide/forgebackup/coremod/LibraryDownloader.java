dc0093bb5dade0c8912321d7de0a04ae801a056c
monoxide <monoxide0184@gmail.com>
2013-01-28 19:43:26 +1100

    Add a copy of the jgit library

--- src/monoxide/forgebackup/coremod/LibraryDownloader.java
+++ src/monoxide/forgebackup/coremod/LibraryDownloader.java
@@ -7,6 +7,7 @@ public class LibraryDownloader implements ILibrarySet {
 	public String[] getLibraries() {
 		return new String[] {
 			"commons-compress-1.4.1.jar",
+			"org.eclipse.jgit-2.2.0.201212191850-r.jar"
 		};
 	}
 
@@ -14,6 +15,7 @@ public class LibraryDownloader implements ILibrarySet {
 	public String[] getHashes() {
 		return new String[] {
 			"a045661155918847e731465645e23ad915d5fc71",
+			"97d0761b9dd618d1f9f6c16c35c3ddf045ba536c",
 		};
 	}
 
