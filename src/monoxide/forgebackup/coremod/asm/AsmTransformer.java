eeed98cab85104c3f1337c90c54661712bb6050e
monoxide <monoxide0184@gmail.com>
2013-01-31 23:33:37 +1100

    Create ASMTransformer superclass

---
+++ src/monoxide/forgebackup/coremod/asm/AsmTransformer.java
@@ -0,0 +1,33 @@
+package monoxide.forgebackup.coremod.asm;
+
+import java.util.Map;
+
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.tree.ClassNode;
+
+import com.google.common.collect.Maps;
+
+import cpw.mods.fml.relauncher.IClassTransformer;
+
+public abstract class AsmTransformer implements IClassTransformer {
+	protected final Map<String, Map<String, String>> mappings = Maps.newHashMap();
+	
+	@Override
+	public final byte[] transform(String name, byte[] bytes) {
+		if (mappings.containsKey(name)) {
+			ClassNode classNode = new ClassNode();
+			ClassReader classReader = new ClassReader(bytes);
+			classReader.accept(classNode, 0);
+			
+			doTransform(classNode, mappings.get(name));
+			
+			ClassWriter writer = new ClassWriter(ClassWriter.COMPUTE_MAXS);
+			classNode.accept(writer);
+			return writer.toByteArray();
+		}
+		return bytes;
+	}
+	
+	protected abstract void doTransform(ClassNode classNode, Map<String, String> mapping);
+}
