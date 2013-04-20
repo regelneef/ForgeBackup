bfaa3f2c6b17091c0c042987dd9a0053da821304
monoxide <monoxide0184@gmail.com>
2013-04-21 00:50:44 +1000

    Fix a minor warning from FML

--- src/monoxide/forgebackup/coremod/ForgeBackupLoader.java
+++ src/monoxide/forgebackup/coremod/ForgeBackupLoader.java
@@ -5,12 +5,14 @@ import java.util.Map;
 import monoxide.forgebackup.BackupLog;
 
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
+import cpw.mods.fml.relauncher.IFMLLoadingPlugin.MCVersion;
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin.TransformerExclusions;
 
 @TransformerExclusions({
 	"monoxide.forgebackup.coremod",
 	"monoxide.forgebackup.coremod.asm",
 })
+@MCVersion("1.5.1")
 public class ForgeBackupLoader implements IFMLLoadingPlugin {
 	@Override
 	public String[] getLibraryRequestClass() {
